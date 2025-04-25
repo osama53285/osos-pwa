0fa5819c8987f68d3ecabce6900fa7ec1107ac8d
osama53285 <osamanasr532@gmail.com>
2025-04-25 13:28:12 +0300

    Update and rename .sw.js to style.css

--- .sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = 'osos-cache-v1';
-const ASSETS = ['/', 'index.html', 'manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-          .then(cache => cache.addAll(ASSETS))
-  );
-});
